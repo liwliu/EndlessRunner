0876295093093dfe5939af795518d9dd3d172279
liwliu <liwliu@ucsc.edu>
2021-07-03 16:32:14 -0700

    added src and lib folders and index.html

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Endless Runner Project
+//
+// Created by:
+// Warren Liu
+// Christorpher Lee
+// John Sanli
+// 
+// Summer 2021
+
+// game configurations
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config);
+
+
+
