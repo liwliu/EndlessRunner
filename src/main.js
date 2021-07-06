bf5570f3c78bd27922597687e86436d96d98b275
liwliu <liwliu@ucsc.edu>
2021-07-06 11:20:01 -0700

    Updated main.js with notes for the Canvas submission

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-// Endless Runner Project
+/* Endless Runner Project
+// 
+// Alien Invasion 
 //
 // Created by:
 // Warren Liu
 // Christorpher Lee
 // John Sanli
-// 
+
+// Completed on July 6th, 2021
 // Summer 2021
+
+// Creative Tilt:
+// - Performs various sound effects upon reactions
+// - Creates different effects whenever you get a high score or not.
+// - Uses a local variable in main in order to track a high score per
+// browser session
+// - Randomly generates platforms and people on each platform to change each session
+// - Music is very classic and sound effects match the style of the game being pixelated and simple
+// - Style is fairly colorful to see and color scheme is great.
+// - Alien being green shows the player's role and stands out from the rest of the background assets.
+*/ 
 
 // game configurations
 
