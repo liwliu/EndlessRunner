7bb856aad156c1cf7fc14fcf4a2541701ada6219
Christopher <clee223@ucsc.edu>
2021-07-05 23:35:13 -0400

    menu graphic added

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,13 @@ class Menu extends Phaser.Scene {
 
     preload() {
         //load audio here
+        this.load.image('menuBackground', './assets/img/endlessRunnerMenu.png');
     }
 
     create() {
+
+        
+
         let menuConfig = {
             fontFamily: 'Impact',
             fontSize: '40px',
@@ -20,9 +24,14 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+
+        //menu background
+        this.newMenu - this.add.tileSprite(0,0, 640, 480, 'menuBackground').setOrigin(0,0);
         
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ALIEN INVASION', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'USE SPACE TO START', menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ALIEN INVASION', menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2, 'USE SPACE TO START', menuConfig).setOrigin(0.5);
+        
+        
         /*menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
